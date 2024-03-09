1c34ca6c1dab0376dbfb8194958a86679dcb2b34
suttha jongjaroen <6410615121@cn330>
2024-03-09 10:36:04 +0700

    update server.js more organized

--- server.js
+++ server.js
@@ -1,23 +1,15 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const router = require('./router');
+const express = require("express");
+const router = require("./router");
 
-const app = express(); 
-const PORT = 3000; 
+const app = express();
+const PORT = 3000;
 
-const sqlite = require("sqlite3").verbose();
-const db = new sqlite.Database("./database/uniconnect.db", sqlite.OPEN_READWRITE, (err) =>{
-    if(err) return console.log(err);
-})
+app.use("/api", router);
 
-app.use('/', router);
-app.use(bodyParser.json())
-
-
-app.listen(PORT, (error) =>{ 
-    if(!error) 
-        console.log("Server is Successfully Running, and App is listening on port " + PORT) 
-    else 
-        console.log("Error occurred, server can't start", error); 
-    } 
-); 
+app.listen(PORT, (error) => {
+  if (!error)
+    console.log(
+      "Server is Successfully Running, and App is listening on port " + PORT
+    );
+  else console.log("Error occurred, server can't start", error);
+});
