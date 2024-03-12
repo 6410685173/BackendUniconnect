9d12717eb16109d84ecf3becb422f8623f0695ec
suttha jongjaroen <6410615121@cn330>
2024-03-12 17:54:15 +0700

    update dependencies

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const courseRouter = require("./routes/courses");
 const authRouter = require("./routes/auth")
+const cors = require('cors');
 
 const app = express();
 const PORT = 3000;
+
+// const corsOptions = {
+//   origin: 'http://localhost:3000', // Allow requests from this origin
+//   methods: 'GET,POST', // Allow these HTTP methods
+//   allowedHeaders: 'Content-Type,Authorization', // Allow these headers
+// };
+app.use(cors());
 
 app.use("/api", courseRouter);
 app.use("/auth", authRouter)
