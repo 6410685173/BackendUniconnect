44b9638104d83659ad6a56fb55743c937236e186
suttha jongjaroen <6410615121@cn330>
2024-03-19 16:58:44 +0700

    clearer status code

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const bcrpyt = require('bcrypt');
+const express = require("express");
+const bcrpyt = require("bcrypt");
 const authRouter = express();
 
 authRouter.use(express.json());
@@ -16,47 +16,52 @@ const db = new sqlite.Database(
 let sql;
 //
 
-// auth 
+// auth
 // auth register
-authRouter.post('/register', async (req,res) => {
-    const { username, password, confirmPassword} = await req.body;
+authRouter.post("/register", async (req, res) => {
+  const { username, password, confirmPassword } = await req.body;
 
-    if(password != confirmPassword){
-        return res.status(400).json({message: "passwords do not match"})
-    }
+  if (password != confirmPassword) {
+    return res.status(400).json({ message: "passwords do not match" });
+  }
 
-    const hashedPassword = await bcrpyt.hash(password, 10)
+  const hashedPassword = await bcrpyt.hash(password, 10);
 
-    sql = `INSERT INTO user(username, password) VALUES(?,?)`;
-    db.run(sql,[username, hashedPassword], (err) => {
-        if(err) return res.status(400).json({error: err.message})
-        console.log(`Created user: ${username}`)
-        return res.status(200).json({message: `Inserted user: ${username}!`})
-    })
-})
+  sql = `INSERT INTO user(username, password) VALUES(?,?)`;
+  db.run(sql, [username, hashedPassword], (err) => {
+    if (err && err.errno === 19) {
+      return res.status(400).json({ error: err.message });
+    }else if(err){
+			return res.status(500).json({ error: err.message });
+		}
+    console.log(`Created user: ${username}`);
+    return res.status(200).json({ message: `Inserted user: ${username}!` });
+  });
+});
 //
 
 // auth login
-authRouter.post('/login',(req,res) => {
-    const {username, password} = req.body;
+authRouter.post("/login", (req, res) => {
+  const { username, password } = req.body;
 
-    // find user
-    sql = `SELECT * FROM user WHERE username = ?`
-    db.get(sql,[username],(err, row) => {
-        if(err) return res.status(500).json({ error: err.message });
-        if(!row) return res.status(404).json({ message: "User not found"})
-        try{
-            if(bcrpyt.compareSync(password, row.password)){
-                console.log(`Logged In user: ${username}`)
-                return res.status(200).json({message: "Logged In!"})
-            }else{
-                return res.status(401).json({message: "Incorrect username or password"})
-            }
-        }catch(err){
-            return res.status(500).json({ error: err.message });
-        }
-    })
-
-})
+  // find user
+  sql = `SELECT * FROM user WHERE username = ?`;
+  db.get(sql, [username], (err, row) => {
+    if (err) return res.status(500).json({ error: err.message });
+    if (!row) return res.status(404).json({ message: "User not found" });
+    try {
+      if (bcrpyt.compareSync(password, row.password)) {
+        console.log(`Logged In user: ${username}`);
+        return res.status(200).json({ message: "Logged In!" });
+      } else {
+        return res
+          .status(401)
+          .json({ message: "Incorrect username or password" });
+      }
+    } catch (err) {
+      return res.status(500).json({ error: err.message });
+    }
+  });
+});
 //
 module.exports = authRouter;
